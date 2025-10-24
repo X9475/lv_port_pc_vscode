a6e09a89555b9d4e8b186d58d52ab79198a9a84a
velpro <velpro@18-04-0827.myguest.virtualbox.org>
2025-10-24 16:47:04 +0800

    修复设置菜单子功能返回点击失效问题

--- lv_child_camera/common/page_src/menu_setting/audio_effect.c
+++ lv_child_camera/common/page_src/menu_setting/audio_effect.c
@@ -201,6 +201,7 @@ static void lv_switch_observer_cb(lv_observer_t *observer, lv_subject_t *subject
     
     if (page_event == PAGE_SWITCH_BACK) {
         lv_obj_clear_flag(lv_page_menu_setting_info_get()->page, LV_OBJ_FLAG_HIDDEN);
+        lv_subject_set_int(&menu_setting_subject, PAGE_SWITCH_NONE);
         lv_obj_del(audio_effect_page_info.page);
         return;
     }
