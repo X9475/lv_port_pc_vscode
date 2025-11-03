268f8706f40fea48973b018ba92b2ce1e9d4befe
velpro <velpro@18-04-0827.myguest.virtualbox.org>
2025-11-03 10:47:57 +0800

    修改认证标志页面内容

--- lv_child_camera/common/page_src/settings/cert_mask.c
+++ lv_child_camera/common/page_src/settings/cert_mask.c
@@ -5,7 +5,6 @@ static lv_switch_page_pt switch_page;
 
 static lv_obj_t *screen = NULL;
 static lv_style_t screen_style;
-static lv_style_t style_mask;
 
 static void lv_page_construct(void *this);
 static void lv_page_destruct(void);
@@ -60,7 +59,6 @@ static void lv_page_construct(void *this)
 static void lv_page_destruct(void)
 {
     lv_style_reset(&screen_style);
-    lv_style_reset(&style_mask);
     lv_page_subject_deinit();
 }
 
@@ -72,22 +70,7 @@ static void lv_page_style_init()
     lv_style_set_pad_all(&screen_style, 0);
     lv_style_set_border_width(&screen_style, 0);
     lv_style_set_bg_color(&screen_style, lv_color_hex(0x000000));
-    lv_style_set_bg_opa(&screen_style, LV_OPA_COVER);
-
-    //图层蒙板
-    static lv_grad_dsc_t grad;
-    grad.dir = LV_GRAD_DIR_VER;
-    grad.stops_count = 2;
-    grad.stops[0].color = lv_color_hex(0x000000);
-    grad.stops[0].opa = LV_OPA_TRANSP;
-    grad.stops[1].color = lv_color_hex(0x000000);
-    grad.stops[1].opa = LV_OPA_COVER;
-    grad.stops[0].frac = 0;
-    grad.stops[1].frac = 255;
-    lv_style_init(&style_mask);
-    lv_style_copy(&style_mask, &screen_style);
-    lv_style_set_bg_grad(&style_mask, &grad);
-    lv_style_set_bg_grad_dir(&style_mask, LV_GRAD_DIR_VER);
+    lv_style_set_bg_opa(&screen_style, LV_OPA_90);
 }
 
 static void lv_page_subject_init()
@@ -112,57 +95,29 @@ static void lv_page_load(lv_obj_t *cont)
     lv_obj_add_flag(back, LV_OBJ_FLAG_CLICKABLE);
     lv_obj_add_event_cb(back, page_back_event_cb, LV_EVENT_CLICKED, NULL);
 
-    //背景图
-    lv_obj_t *image = lv_img_create(cont);
-    lv_img_set_src(image, "../lv_port_pc_vscode/assert/icon/setting_icon_legal_notice.png");
-    lv_obj_set_size(image, 140, 140);
-    lv_obj_align_to(image, cont, LV_ALIGN_TOP_MID, 0, 20);
+    lv_obj_t *description = lv_obj_create(cont);
+    lv_obj_set_size(description, 450, 180);
+    lv_obj_add_style(description, &screen_style, 0);
+    lv_obj_clear_flag(description, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_style_radius(description, 18, LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(description, LV_OPA_COVER, 0);
+    lv_obj_align(description, LV_ALIGN_CENTER, 0, 0);
 
-    //文字提示：认证标识
-    lv_obj_t *label = lv_label_create(cont);
-    lv_label_set_text(label, "认证标识");
+    const char *text = "CMIIT ID：XXXXXXXXXXXX\n版本：V100-1M88SF4G-Z\nTD-LTE无线数据终端\n中国制造";
+    lv_obj_t *label = lv_label_create(description);
+    lv_label_set_text(label, text);
     lv_obj_set_style_text_opa(label, LV_OPA_COVER, 0);
-    lv_obj_set_style_text_font(label, fzlthr_40, 0);
+    lv_obj_set_style_text_font(label, fzlthr_22, 0);
     lv_obj_set_style_text_color(label, lv_color_hex(0xFFFFFF), 0);
-    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 147);
+    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_LEFT, 0);
+    lv_obj_align(label, LV_ALIGN_TOP_LEFT, 30, 35);
 
-    //创建滚动容器作为文本的父容器
-    lv_obj_t *scroll_cont = lv_obj_create(cont);
-    lv_obj_set_size(scroll_cont, 422, 201);
-    lv_obj_add_style(scroll_cont, &screen_style, LV_PART_MAIN);
-    lv_obj_align(scroll_cont, LV_ALIGN_BOTTOM_MID, 0, 0);
-    lv_obj_set_flex_flow(scroll_cont, LV_FLEX_FLOW_COLUMN);
-
-    //创建长文本标签
-    lv_obj_t *description = lv_label_create(scroll_cont);
-    lv_label_set_long_mode(description, LV_LABEL_LONG_WRAP);
-    lv_obj_set_width(description, lv_pct(100));
-    lv_obj_set_style_text_font(description, fzlthr_28, 0);
-    lv_obj_set_style_text_opa(description, LV_OPA_60, 0);
-    lv_obj_set_style_text_color(description, lv_color_hex(0xEBEBF5), 0);
-    lv_obj_set_style_text_align(description, LV_TEXT_ALIGN_CENTER, 0);
-
-    //设置长文本内容
-    const char *long_text = "Are you sure to exit the level calibration process?"
-                            "Are you sure to exit the level calibration process?"
-                            "Are you sure to exit the level calibration process?"
-                            "Are you sure to exit the level calibration process?"
-                            "Are you sure to exit the level calibration process?"
-                            "Are you sure to exit the level calibration process?"
-                            "Are you sure to exit the level calibration process?";
-    lv_label_set_text(description, long_text);
-
-    // 确保内容可以滚动
-    lv_obj_set_scroll_dir(scroll_cont, LV_DIR_VER);
-    lv_obj_set_scrollbar_mode(scroll_cont, LV_SCROLLBAR_MODE_AUTO);
-
-    //文字蒙层
-    lv_obj_t *mask = lv_obj_create(cont);
-    lv_obj_add_style(mask, &style_mask, 0);
-    lv_obj_set_size(mask, lv_pct(100), 100);
-    lv_obj_align(mask, LV_ALIGN_BOTTOM_MID, 0, 0);
-    lv_obj_clear_flag(mask, LV_OBJ_FLAG_CLICKABLE);//禁止拦截点击事件
+    //环保标志
+    lv_obj_t *image = lv_img_create(description);
+    lv_obj_set_size(image, 100, 100);
+    lv_img_set_src(image, "../lv_port_pc_vscode/assert/icon/environment_protect_symbol_10.png");
+    lv_img_set_zoom(image, 200);
+    lv_obj_align(image, LV_ALIGN_BOTTOM_RIGHT, -20, -20);
 
     return;
 }
