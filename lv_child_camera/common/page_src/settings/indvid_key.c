8b7be279a30f53194f835d8bb2526de851adf2e2
velpro <velpro@18-04-0827.myguest.virtualbox.org>
2026-03-04 19:10:05 +0800

    修改自定义按键界面内容

--- lv_child_camera/common/page_src/settings/indvid_key.c
+++ lv_child_camera/common/page_src/settings/indvid_key.c
@@ -5,6 +5,8 @@ static lv_switch_page_pt switch_page;
 
 static lv_obj_t *screen = NULL;
 static lv_style_t screen_style;
+static lv_style_t up_style;
+static lv_style_t bottom_style;
 static int indvid_key_select = 0;
 
 typedef struct
@@ -24,7 +26,6 @@ static void lv_page_subject_init();
 static void lv_page_subject_deinit();
 static void lv_page_load(lv_obj_t *cont);
 static void lv_switch_observer_cb(lv_observer_t *observer, lv_subject_t *subject);
-static void btn_confirm_click_event(lv_event_t *e);
 static void page_back_event_cb(lv_event_t *e);
 static void indvid_key_iterm_create(lv_obj_t *cont_col, indvid_key_info_t *indvid_key, int i);
 static void indvid_key_click_event_cb(lv_event_t *e);
@@ -70,6 +71,8 @@ static void lv_page_construct(void *this)
 
 static void lv_page_destruct(void)
 {
+    lv_style_reset(&up_style);
+    lv_style_reset(&bottom_style);
     lv_style_reset(&screen_style);
     lv_page_subject_deinit();
 }
@@ -83,6 +86,32 @@ static void lv_page_style_init()
     lv_style_set_border_width(&screen_style, 0);
     lv_style_set_bg_color(&screen_style, lv_color_hex(0x000000));
     lv_style_set_bg_opa(&screen_style, LV_OPA_COVER);
+
+    static lv_grad_dsc_t grad0;
+    grad0.dir = LV_GRAD_DIR_VER;
+    grad0.stops_count = 2;
+    grad0.stops[0].color = lv_color_hex(0x000000);
+    grad0.stops[0].opa = LV_OPA_COVER;
+    grad0.stops[1].color = lv_color_hex(0x000000);
+    grad0.stops[1].opa = LV_OPA_TRANSP;
+    grad0.stops[0].frac = 0;
+    grad0.stops[1].frac = 255;
+    lv_style_init(&up_style);
+    lv_style_copy(&up_style, &screen_style);
+    lv_style_set_bg_grad(&up_style, &grad0);
+
+    static lv_grad_dsc_t grad1;
+    grad1.dir = LV_GRAD_DIR_VER;
+    grad1.stops_count = 2;
+    grad1.stops[0].color = lv_color_hex(0x000000);
+    grad1.stops[0].opa = LV_OPA_TRANSP;
+    grad1.stops[1].color = lv_color_hex(0x000000);
+    grad1.stops[1].opa = LV_OPA_COVER;
+    grad1.stops[0].frac = 0;
+    grad1.stops[1].frac = 255;
+    lv_style_init(&bottom_style);
+    lv_style_copy(&bottom_style, &screen_style);
+    lv_style_set_bg_grad(&bottom_style, &grad1);
 }
 
 static void lv_page_subject_init()
@@ -120,35 +149,51 @@ static void lv_page_load(lv_obj_t *cont)
     lv_obj_set_style_text_align(header, LV_TEXT_ALIGN_CENTER, 0);
     lv_obj_align_to(header, back_btn, LV_ALIGN_OUT_RIGHT_MID, -8, 0);
 
+    lv_obj_t *contain = lv_obj_create(cont);
+    lv_obj_set_size(contain, lv_pct(100), 297);
+    lv_obj_add_style(contain, &screen_style, 0);
+    lv_obj_set_scroll_dir(contain, LV_DIR_VER);
+    lv_obj_set_scrollbar_mode(contain, LV_SCROLLBAR_MODE_OFF);
+    lv_obj_align(contain, LV_ALIGN_TOP_MID, 0, 113);
+
+    lv_obj_t *product_img = lv_img_create(contain);
+    lv_obj_set_size(product_img, 264, 157);
+    lv_img_set_src(product_img, "../lv_port_pc_vscode/assert/icon/key_yellow.png");
+    lv_obj_align(product_img, LV_ALIGN_TOP_MID, 0, 0);
+
+    //上部渐变
+    lv_obj_t *up_mask = lv_obj_create(contain);
+    lv_obj_set_size(up_mask, 474, 37);
+    lv_obj_add_style(up_mask, &up_style, 0);
+    lv_obj_align(up_mask, LV_ALIGN_TOP_MID, 0, 0);
+    //下部渐变
+    lv_obj_t *bottom_mask = lv_obj_create(contain);
+    lv_obj_set_size(bottom_mask, 474, 72);
+    lv_obj_add_style(bottom_mask, &bottom_style, 0);
+    lv_obj_align(bottom_mask, LV_ALIGN_TOP_MID, 0, 85);
+
+    lv_obj_t *descrip = lv_label_create(contain);
+    lv_label_set_text(descrip, "你想通过此按键");
+    lv_obj_set_style_text_font(descrip, fzlthr_24, 0);
+    lv_obj_set_style_text_opa(descrip, LV_OPA_60, 0);
+    lv_obj_set_style_text_color(descrip, lv_color_hex(0xEBEBF5), 0);
+    lv_obj_set_style_text_align(descrip, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_align(descrip, LV_ALIGN_TOP_LEFT, 40, 200);
+
     //滚动列表
-    lv_obj_t *cont_col = lv_obj_create(cont);
-    lv_obj_set_size(cont_col, lv_pct(100), 332);
+    lv_obj_t *cont_col = lv_obj_create(contain);
+    lv_obj_set_size(cont_col, lv_pct(100), 256);
     lv_obj_add_style(cont_col, &screen_style, 0);
     lv_obj_set_scroll_dir(cont_col, LV_DIR_VER);
     lv_obj_set_flex_flow(cont_col, LV_FLEX_FLOW_COLUMN);
     lv_obj_set_scrollbar_mode(cont_col, LV_SCROLLBAR_MODE_OFF);
-    lv_obj_align_to(cont_col, cont, LV_ALIGN_TOP_MID, 40, 78);
+    lv_obj_set_flex_align(cont_col, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER);
+    lv_obj_align(cont_col, LV_ALIGN_TOP_MID, 0, 231);
 
     for (uint8_t i = 0; i < 2; i++)
     {
         indvid_key_iterm_create(cont_col, &g_indvid_key_info[i], i);
     }
-
-    lv_obj_t *confirm = lv_btn_create(cont);
-    lv_obj_set_size(confirm, 148, 70);
-    lv_obj_set_style_radius(confirm, 51, 0);
-    lv_obj_set_style_bg_opa(confirm, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_color(confirm, lv_color_hex(0xAFF99C), 0);
-    lv_obj_set_style_shadow_width(confirm, 0, 0);
-    lv_obj_align(confirm, LV_ALIGN_BOTTOM_MID, 0, -12);
-
-    lv_obj_t *icon = lv_img_create(confirm);
-    lv_obj_set_size(icon, 50, 50);
-    lv_img_set_src(icon, "../lv_port_pc_vscode/assert/icon/common_icon_ok_button.png");
-    lv_obj_align_to(icon, confirm, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_set_style_image_recolor_opa(icon, LV_OPA_COVER, 0);
-    lv_obj_set_style_image_recolor(icon, lv_color_hex(0x0A0B0D), 0);
-    lv_obj_add_event_cb(confirm, btn_confirm_click_event, LV_EVENT_CLICKED, confirm);
 
     return;
 }
@@ -218,23 +263,13 @@ static void indvid_key_click_event_cb(lv_event_t *e)
         if (checkbox == lv_obj_get_child(cont_col, 0) && indvid_key_select)
         {
             indvid_key_select = 0;
+            printf("拍摄\n");
         }
         else if (checkbox == lv_obj_get_child(cont_col, 1) && !indvid_key_select)
         {
             indvid_key_select = 1;
+            printf("AI问答\n");
         }
-    }
-}
-
-static void btn_confirm_click_event(lv_event_t *e)
-{
-    if (indvid_key_select == 0)
-    {
-        printf("拍摄\n");
-    }
-    else if (indvid_key_select == 1)
-    {
-        printf("AI问答\n");
     }
 }
 
